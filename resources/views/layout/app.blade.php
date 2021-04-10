729e60bebb3519a3a2795b7d8156e4d434c65c21
Lucile Vacquie <lucilevacquie@Luciles-MacBook-Pro.local>
2021-04-10 13:31:56 +0100

    navbar

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,9 +4,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset("css/app.css")}}"/>    
     <title>Post</title>
 </head>
-<body>
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between">
+        <ul class="flex items-center">
+            <li class="p-3">
+                <a href="">Home</a>
+            </li>
+            <li class="p-3">
+                <a href="">Dashboard</a>
+            </li>
+            <li class="p-3">
+                <a href="">Post</a>
+            </li>
+        </ul>
+        <ul class="flex items-center">
+            <li class="p-3">
+                <a href="">Login</a>
+            </li>
+            <li class="p-3">
+                <a href="">Register</a>
+            </li>
+            <li class="p-3">
+                <a href="">Logout</a>
+            </li>
+        </ul>
+    </nav>
     @yield("content")
 </body>
 </html>
